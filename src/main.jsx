e246e81cf70761424df43d7d75f2e90b14225e65
RKafka <rkafka14@gmail.com>
2025-03-12 14:54:54 -0500

    file system structure reorganization

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,13 @@ import App from './App.jsx';
 // sans-serif
 import "@fontsource/outfit";
 import "@fontsource/roboto";
+import "@fontsource/oswald";
+import "@fontsource/montserrat";
 // serif
 import "@fontsource/roboto-slab";
 import "@fontsource/libre-baskerville";
 import "@fontsource/playfair-display";
+import "@fontsource/dm-serif-display";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
